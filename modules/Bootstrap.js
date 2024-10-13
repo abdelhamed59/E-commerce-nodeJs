1f057446f2fcffd35f2a37a54caf19ff46df6762
abdelhamed59 <abdelhamed59@gmail.com>
2024-10-14 00:11:25 +0300

    vecel environment

--- modules/Bootstrap.js
+++ modules/Bootstrap.js
@@ -1,6 +1,6 @@
 import authRoutes from "./auth/auth.routes.js";
+import brandRoutes from "./Brand/brand.routes.js";
 import cartRoutes from "./Cart/cart.routes.js";
-import brandRoutes from "./Brand/brand.routes.js"
 import categoryRoutes from "./category/category.routes.js"
 import couponRoutes from "./coupon/coupon.routes.js";
 import orderRoutes from "./order/order.routes.js";
@@ -14,8 +14,8 @@ import wishListRoutes from "./wishList/wishList.routes.js";
 export const Bootstrap=(app)=>{
     app.use("/api/v1/categories",categoryRoutes);
     app.use("/api/v1/subCategories",subCategoryRoutes);
-    app.use("/api/v1/brands",brandRoutes);
     app.use("/api/v1/products",productRoutes);
+    app.use("/api/v1/brands",brandRoutes)
     app.use("/api/v1/users",userRoutes);
     app.use("/api/v1/auth",authRoutes);
     app.use("/api/v1/review",reviewRoutes);
--- modules/Brand/Brand.routes.js
+++ modules/Brand/Brand.routes.js
@@ -2,19 +2,16 @@ import express from 'express'
 import { validation } from '../../middleware/validation.js'
 import { uploadSingle } from '../../utili/fileUpload.js'
 import { addBrand, deleteBrand, getBrand, getBrands, updateBrand } from './Brand.controller.js'
-import { brandSchema, updateBrandSchema ,getByIdSchema} from './brand.validation.js'
+import { brandSchema, updateBrandSchema, getByIdSchema } from './brand.validation.js'
 
-const brandRoutes = express.Router()
-
+const brandRoutes=express.Router()
 brandRoutes.route("/")
     .post(uploadSingle("image"), validation(brandSchema), addBrand)
     .get(getBrands)
 
-    brandRoutes.route("/:id")
-    .put(uploadSingle("image"),validation(updateBrandSchema),updateBrand)
+brandRoutes.route("/:id")
+    .put(uploadSingle("image"), validation(updateBrandSchema), updateBrand)
     .get(validation(getByIdSchema), getBrand)
     .delete(validation(getByIdSchema), deleteBrand)
-
-
 
 export default brandRoutes

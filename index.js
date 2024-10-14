b1302b327d8bd58fcde7b0793272411d64669c13
abdelhamed59 <abdelhamed59@gmail.com>
2024-10-14 05:06:51 +0300

    error

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import { Bootstrap } from './modules/Bootstrap.js'
 import AppError from './utili/appError.js'
 import cors from 'cors'
 import handleError from './middleware/handelAsyncError.js'
+// import { Cart } from './DB/models/cart.model.js'
+// import { Order } from './DB/models/order.model.js'
+// import { User } from './DB/models/user.model.js'
 import Stripe from 'stripe';
-import { Cart } from './DB/models/cart.model.js'
-import { Order } from './DB/models/order.model.js'
-import { User } from './DB/models/user.model.js'
 const stripe = new Stripe('sk_test_51Q9CVQRs8NJMQca17tLchooppuaNxOixtIlyDTRlo6n6HH1SKNVnNU8RK1795AOPgUMPLBFA9SbnjNxO1ye2Hp2Z00cmvQxk3g');
 
 const app = express()
@@ -27,35 +27,33 @@ console.log(event);
 if(event.type=="checkout.session.completed"){
   checkout = event.data.object;
 
-  let user=await User.fiudOne({email:checkout.customer_email})
+  // let user=await User.fiudOne({email:checkout.customer_email})
 
+  // let cart=await Cart.findById({_id:checkout.client_reference_id})
+  // let order=new Order({
+  //   user:user._idid,
+  //   cartItems:cart.cartItems,
+  //   totalPrice:checkout.amount_total/100,
+  //   shippingAddress:checkout.metadata,
+  //   paymentMethod:"online",
+  //   isPaid:true,
+  //   paidAt:Date.now()
+  // })
+  // await order.save()
 
+  // if(order){
+  //   let options= cart.cartItems.map(ele=>({
+  //       updateOne:{
+  //           filter:{_id:ele.product},
+  //           update:{$inc:{quantity:-ele.quantity,sold:ele.quantity}}
+  //       }
+  //   }))
 
-  let cart=await Cart.findById({_id:checkout.client_reference_id})
-  let order=new Order({
-    user:user._idid,
-    cartItems:cart.cartItems,
-    totalPrice:checkout.amount_total/100,
-    shippingAddress:checkout.metadata,
-    paymentMethod:"online",
-    isPaid:true,
-    paidAt:Date.now()
-  })
-  await order.save()
-
-  if(order){
-    let options= cart.cartItems.map(ele=>({
-        updateOne:{
-            filter:{_id:ele.product},
-            update:{$inc:{quantity:-ele.quantity,sold:ele.quantity}}
-        }
-    }))
-
-    await Product.bulkWrite(options)
-  }else{
-    return next(new AppError("oder occurs",409))
-  }
-  await Cart.findByIdAndDelete(req.params.id)
+  //   await Product.bulkWrite(options)
+  // }else{
+  //   return next(new AppError("oder occurs",409))
+  // }
+  // await Cart.findByIdAndDelete(req.params.id)
 }
   res.status(200).json({message:"success",checkout});
 }));
